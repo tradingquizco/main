7fefd915462c1b3163e584642edac3eb3ae18fc8
mahdi <mahdi.funlife@gmail.com>
2024-10-17 14:22:11 +0330

    BUG - fix UI bug

--- app/page.tsx
+++ app/page.tsx
@@ -6,45 +6,52 @@ const Home = () => {
   return (
     <div className="h-dvh relative pt-3 ">
       <Header />
-      <div className=" absolute top-[15%] flex items-center justify-around gap-10 w-full max-lg:flex-col">
+      <div className=" absolute top-[15%] flex items-center justify-around gap-10 w-full max-lg:flex-col max-lg:top-[20%] ">
         <div className="uppercase flex flex-col items-center gap-2">
-          <h2 className="text-5xl font-extrabold max-lg:text-3xl">tradeing quiz </h2>
-          <span className="font-light text-3xl text-slate-700">be a real hunter</span>
-          <div className="bg-white shadow-md rounded-md p-2 flex gap-2">
-            <input type="text" placeholder="Enter youre Email" className="bg-transparent outline-none"/>
-            <Button>Sign Up</Button>
-          </div>
+          <h2 className="text-7xl font-extrabold max-lg:text-3xl">
+            tradeing quiz{" "}
+          </h2>
+          <span className="font-light text-4xl text-slate-700">
+            be a real hunter
+          </span>
+
+          <button className="bg-orange-400 shadow-orange-400 p-2 shadow-md text-white rounded-md px-2  hover:shadow-lg hover:shadow-orange-400 transition-all">
+            Get Started
+          </button>
         </div>
-        <img src="./test.png" alt="" className="max-w-96 " />
-        <button className="bg-orange-400 shadow-orange-400 p-2 shadow-md text-white rounded-md px-2 max-lg:hidden">Get Started</button>
+        <img src="./test.png" alt="" className="max-w-2xl max-md:max-w-96 max-sm:max-w-80" />
       </div>
-      <img src="./Frame.png" alt="" className="max-w-96" />
+      <img src="./Frame.png" alt="" className="max-w-xl max-md:max-w-72" />
 
-      <div className="flex justify-around max-xl:flex-col max-xl:items-center max-xl:gap-5">
-        <div className="bg-gray-200 rounded-md p-5 flex flex-col gap-5 max-w-80 shadow-md shadow-gray-600">
-          <h4 className="text-center uppercase font-semibold">follower <span>+5000</span></h4>
-          <p className="text-slate-600 text-sm text-center font-medium">Join the big Trading Quiz family by following us</p>
-          <Button>Joining US</Button>
+      <div className="flex justify-around max-xl:flex-col max-xl:mt-14 max-xl:items-center max-xl:gap-5 ">
+        <div className="p-5 flex flex-col gap-5 items-center max-w-80 cursor-pointer transition ease-linear delay-75 hover:-translate-y-4">
+          <h4 className="uppercase text-3xl font-semibold">follower</h4>
+          <span className="font-medium text-2xl text-orange-400">+5000</span>
         </div>
 
-        <div className="bg-gray-200 rounded-md p-5 flex flex-col gap-5 max-w-80 shadow-md shadow-gray-600">
-          <h4 className="text-center uppercase font-semibold">Quiz <span>+67000</span></h4>
-          <p className="text-slate-600 text-sm text-center font-medium">Join the big Trading Quiz family by following us</p>
-          <Button>Joining US</Button>
+        <div className="p-5 flex flex-col gap-5 items-center max-w-80 cursor-pointer transition ease-linear delay-75 hover:-translate-y-4">
+          <h4 className="uppercase text-3xl font-semibold">online user</h4>
+          <span className="font-medium text-2xl text-orange-400">+2219</span>
         </div>
 
-        <div className="bg-gray-200 rounded-md p-5 flex flex-col gap-5 max-w-80 shadow-md shadow-gray-600">
-          <h4 className="text-center uppercase font-semibold">online user <span>+4514</span></h4>
-          <p className="text-slate-600 text-sm text-center font-medium">Join the big Trading Quiz family by following us</p>
-          <Button>Joining US</Button>
+        <div className="p-5 flex flex-col gap-5 items-center max-w-80 cursor-pointer transition ease-linear delay-75 hover:-translate-y-4">
+          <h4 className="uppercase text-3xl font-semibold">Quiz</h4>
+          <span className="font-medium text-2xl text-orange-400">+7300</span>
         </div>
-
       </div>
 
-      <div className="mb-14 mt-40 flex justify-around max-lg:flex-col max-lg:items-center max-lg:mt-20">
-        <textarea placeholder="Example: woow!!" className="bg-gray-200 outline-none rounded-md resize-none p-2 w-64 h-32 shadow-gray-400 shadow-md"/>
-        <h5 className="text-2xl font-bold text-center leading-10 max-lg:text-xl max-lg:my-5">Your criticism and suggestions will encourage and improve us.<br/> Thank you for your feedback<br/><Button>Submit</Button></h5>
-      </div>
+      {/* <div className="mb-14 mt-40 flex justify-around max-lg:flex-col max-lg:items-center max-lg:mt-20">
+        <textarea
+          placeholder="Example: woow!!"
+          className="bg-gray-200 outline-none rounded-md resize-none p-2 w-64 h-32 shadow-gray-400 shadow-md"
+        />
+        <h5 className="text-2xl font-bold text-center leading-10 max-lg:text-xl max-lg:my-5">
+          Your criticism and suggestions will encourage and improve us.
+          <br /> Thank you for your feedback
+          <br />
+          <Button>Submit</Button>
+        </h5>
+      </div> */}
     </div>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,21 +3,17 @@ import React from "react";
 const Header = () => {
   return (
     <header className="flex justify-around items-center mt-2 max-lg:mt-10">
-      <div className="bg-red-300 w-11 max-lg:max-w-60">
-        <img src="./logo.png" alt="" className="" />
+      <div className="flex items-center underline font-bold tracking-wider">
+        <img src="./logo.png" alt="" className="w-12 max-lg:max-w-60" />
+        <span className="max-xl:hidden">Treading Quiz</span>
       </div>
       <div className="flex gap-10">
-        <a href="/">Home</a>
+        <a href="/" className="hover:underline text-lg">Home</a>
       </div>
-      <div className="flex gap-3 text-sm ">
-        <button className="bg-orange-400 shadow-orange-400 shadow-md text-white rounded-md px-2 pt-1 h-fit">
-          Sign up
-        </button>
-        <button className="border-orange-400 border   rounded-md px-2 pt-1 h-fit">
-          Login
-        </button>
-        
-      </div>
+
+      <button className="bg-orange-400 shadow-orange-400 shadow-md text-white rounded-md   py-2 px-3 ">
+        Sign up
+      </button>
     </header>
   );
 };
